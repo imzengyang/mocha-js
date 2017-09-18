a2a0284300831dd49b9893ccc37a725d5daf4861
zack <zengyangzhao@outlook.com>
2017-09-18 08:39:56 +0800

    fix windows runtime bugs

--- test.js
+++ test.js
@@ -49,7 +49,7 @@ describe('hooks', function () {
         });
 
         it('导航到登录页面', async function () {
-            await web.get("http://localhost:3000")
+            await web.get("http://192.168.219.129:3000")
         });
         it('最大化窗口', async function () {
             await web.manage().window().maximize()
@@ -73,37 +73,6 @@ describe('hooks', function () {
         });
         it('点击登录', async function () {
             await web.findElement(by.id('pass')).submit();
-        });
-
-        it('点击发布话题进入编辑界面', async function () {
-            let geturl = await web.getCurrentUrl();
-            console.log("geturl", geturl);
-            await web.findElement(by.className('span-success')).click()
-        });
-        it('选择模块', async function () {
-            await web.findElement(by.id('tab-value')).click()
-            await web.findElement(by.css('#tab-value > option:nth-child(2)')).click()    // //选择第二行的元素
-        });
-        it('输入标题', async function () {
-            await web.findElement(by.id('title')).sendKeys('交友聊天相亲招聘交流群')                                 //输入标题
-        });
-        it('输入正文', async function () {
-            await web.findElement(by.css('.CodeMirror.cm-s-paper')).click()
-            let txt = web.findElement(by.xpath('//*[@id="create_topic_form"]/fieldset/div/div/div[2]/div[6]/div[2]'))
-            await web.actions().mouseMove(txt).sendKeys("可以在楼下回复如：你好啊，交个朋友吧").perform()
-        });
-        it('添加网址链接', async function () {
-            //定位正文元素 位置并且输入文字
-            await web.findElement(by.className("eicon-link")).click()
-            await web.sleep(1000)
-            await web.findElement(by.xpath('/html/body/div[4]/div[2]/form/div[1]/div/input')).sendKeys('百度')
-            await web.findElement(by.xpath('/html/body/div[4]/div[2]/form/div[2]/div/input')).sendKeys('www.baidu.com')
-            await web.findElement(by.className("btn btn-primary")).click()
-        });
-        it('添加图片', async function () {
-            web.actions().mouseMove(web.findElement(by.css('.eicon-image'))).click().perform();
-            // await web.findElement(by.css('.eicon-image')).click()
-            await web.findElement(by.name('file')).sendKeys(picpath)
         });
 
     })
